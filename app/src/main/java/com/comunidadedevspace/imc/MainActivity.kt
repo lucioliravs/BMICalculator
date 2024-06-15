d57e88bd7efcdb12c11a5b5804bf116af36a3322
Lucio Lira <lucio.vieira132@hotmail.com>
2024-06-15 09:08:19 +0100

    Tela inicial editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,25 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.editTextWeight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.editTextHeight)
+        val btnCalculate = findViewById<Button>(R.id.button_calculate)
+
+       btnCalculate.setOnClickListener {
+            val weight = edtWeight.text
+            val height = edtHeight.text
+            Toast.makeText(this, "Peso: $weight Altura: $height", Toast.LENGTH_SHORT).show()
+       }
     }
 }
